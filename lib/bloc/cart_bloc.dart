9699d9c01979f86eb662a0c825bd3bbcc84138ad
burillu <burillu90@hotmail.it>
2024-09-13 16:16:20 +0200

    add bloc listener,  using bloc in home page

--- lib/bloc/cart_bloc.dart
+++ lib/bloc/cart_bloc.dart
@@ -2,15 +2,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marios_pizza/model/product_model.dart';
 
 class CartBloc extends Bloc<CartBlocEvent, CartBlocState> {
-  CartBloc() : super(CartBlocStateContent([]));
+  CartBloc() : super(CartBlocStateLoading()) {
+    on<CartBlocEventInit>((event, emit) async {
+      emit(CartBlocStateLoading());
+      await Future.delayed(Duration(seconds: 2));
+      emit(CartBlocStateLoaded(productsList: products, cart: []));
+    });
+  }
 }
 
 abstract class CartBlocEvent {}
 
+class CartBlocEventInit extends CartBlocEvent {}
+
 abstract class CartBlocState {}
 
-class CartBlocStateContent extends CartBlocState {
-  List<ProductModel> products;
+class CartBlocStateLoading extends CartBlocState {}
 
-  CartBlocStateContent(this.products);
+class CartBlocStateLoaded extends CartBlocState {
+  List<ProductModel> productsList;
+  List<ProductModel> cart = [];
+
+  CartBlocStateLoaded({required this.productsList, required this.cart});
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marios_pizza/bloc/cart_bloc.dart';
 import 'package:marios_pizza/model/product_model.dart';
 import 'package:marios_pizza/theme/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    BlocProvider.of<CartBloc>(context).add(CartBlocEventInit());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ThemeColors.bgScaffold,
-      floatingActionButton: IconButton(
-        iconSize: 35,
-        style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(ThemeColors.bgAppBar),
-            minimumSize: WidgetStateProperty.all(Size(60, 60))),
-        icon: Icon(Icons.shopping_cart_outlined),
-        onPressed: () {
-          Navigator.pushNamed(context, "/cart");
-        },
-      ),
+      floatingActionButton: floatingButton(),
       appBar: appBar(),
       body: sectionProduct(),
     );
@@ -46,64 +51,93 @@ class HomePage extends StatelessWidget {
   }
 
   Widget sectionProduct() {
-    return GridView.builder(
-        padding: EdgeInsets.fromLTRB(12, 50, 12, 100),
-        itemCount: products.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            childAspectRatio: 3 / 4,
-            crossAxisCount: 2,
-            crossAxisSpacing: 12,
-            mainAxisSpacing: 12),
-        itemBuilder: (context, index) {
-          return Container(
-            padding: EdgeInsets.all(14),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(16)),
-            margin: EdgeInsets.all(2),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            products[index].imageUrl,
-                          ),
-                          fit: BoxFit.cover,
-                        )),
-                    // child:
+    return BlocBuilder<CartBloc, CartBlocState>(builder: (context, state) {
+      if (state is CartBlocStateLoading) {
+        return Center(child: CircularProgressIndicator());
+      }
+      final productsInCart = (state as CartBlocStateLoaded).cart;
+      final productsList = state.productsList;
+      return GridView.builder(
+          padding: EdgeInsets.fromLTRB(12, 50, 12, 100),
+          itemCount: products.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 3 / 4,
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12),
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.all(14),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(16)),
+              margin: EdgeInsets.all(2),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              productsList[index].imageUrl,
+                            ),
+                            fit: BoxFit.cover,
+                          )),
+                      // child:
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  products[index].name,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  products[index].price.toString(),
-                  style: TextStyle(color: Colors.black38),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  minWidth: double.infinity,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.black26),
-                    borderRadius: BorderRadius.circular(16),
+                  SizedBox(
+                    height: 5,
                   ),
-                  child: Text("Seleziona"),
-                ),
-              ],
-            ),
-          );
-        });
+                  Text(
+                    productsList[index].name,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    productsList[index].price.toString(),
+                    style: TextStyle(color: Colors.black38),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    minWidth: double.infinity,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black26),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Text("Seleziona"),
+                  ),
+                ],
+              ),
+            );
+          });
+    });
+  }
+
+  Widget floatingButton() {
+    return BlocBuilder<CartBloc, CartBlocState>(builder: (context, state) {
+      if (state is CartBlocStateLoading) {
+        return Center(child: CircularProgressIndicator());
+      }
+      final productsInCart = (state as CartBlocStateLoaded).cart;
+      if (productsInCart.isEmpty) {
+        return SizedBox();
+      }
+      return IconButton(
+        iconSize: 35,
+        style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(ThemeColors.bgAppBar),
+            minimumSize: WidgetStateProperty.all(Size(60, 60))),
+        icon: Icon(Icons.shopping_cart_outlined),
+        onPressed: () {
+          Navigator.pushNamed(context, "/cart");
+        },
+      );
+    });
   }
 }
